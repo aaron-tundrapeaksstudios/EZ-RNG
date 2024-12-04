333bb2eddee34585240aba02df35f3418a46d9d8
aaron-tundrapeaksstudios <aaron@tundrapeaks.com>
2024-12-03 19:07:02 -0800

    Initial commit
    Added source files

---
+++ ez_rng.gd
@@ -0,0 +1,8 @@
+@tool
+extends EditorPlugin
+
+func _enable_plugin() -> void:
+	add_autoload_singleton("EZRNG", "res://addons/ez_rng/ezrng.tscn")
+
+func _disable_plugin() -> void:
+	remove_autoload_singleton("EZRNG")
