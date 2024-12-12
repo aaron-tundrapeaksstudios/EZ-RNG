37c9216722d473eeeb11ca75049310a2018fc58f
aaron-tundrapeaksstudios <aaron@tundrapeaks.com>
2024-12-12 09:53:32 -0800

    Update ez_rng.gd

--- ez_rng.gd
+++ ez_rng.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enable_plugin() -> void:
-	add_autoload_singleton("EZRNG", "res://addons/ez_rng/ezrng.tscn")
+	add_autoload_singleton("EZRNG", "res://addons/ez_rng/ezrng.gd")
 
 func _disable_plugin() -> void:
 	remove_autoload_singleton("EZRNG")
